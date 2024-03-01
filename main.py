dcfe95c7351d2583d0b75197ea78be49d517fbd7
ALUNO <goncalves.leonardo1@aluno.ifsp.edu.br>
2024-02-29 21:08:57 -0300

    atualização

--- main.py
+++ main.py
@@ -1,56 +1,57 @@
-from sqlalchemy import create_engine, String, DateTime, func
+from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, relationship
-from sqlalchemy import Column, Uuid, uuid
+from sqlalchemy import Column, Uuid, uuid, String, DateTime, func, DECIMAL, INTEGER, Boolean, ForeignKey
 
 
-motor = create_engine("sqlite=pysqlite://banco_de_dados.sqlite",
-                      echo=True)
+
+
+motor = create_engine("sqlite+pysqlite://banco_de_dados.sqlite", echo=True)
+
 
 
 class Base(DeclarativeBase):
     pass
 
 
-class Colunm:
-    pass
-
-
-class Func:
-    pass
-
-
-class DatasMixin:
-    pass
-
-
-class Categorias(Base, DatasMixin):
-    __tablename__ = "tbl_categorias"
+class DatasMixin():
+    dta_cadastro = Column(DateTime,
+                          server_default=func.now(),
+                          nullable=False)
+    dta_atualizacao = Column(DateTime,
+                             onupdate=func.now(),
+                             default=func.now(),
+                             nullable=False)
+class Categoria(Base, DatasMixin):
+    __tablename__ = 'tbl_categorias'
     id = Column(Uuid(as_uuid=True),
-         primary_key=True,
-         default=uuid.uuid4)
+                primary_key=True,
+                default=uuid.uuid4)
+    nome = Column(String(256),
+                 nullable=False)
 
+    listas_de_produtos = relationship("Produto",
+                                      back_populates="categoria",
+                                      cascade="all, delete-orphan",
+                                      lazy="selectin")
+
+
+class Produto(Base, DatasMixin):
+    __tablename__ = 'tbl_produtos'
+
+    id = Column(Uuid(as_uuid=True),
+                primary_key=True,
+                default=uuid.uuid4)
     nome = Column(String(256),
                   nullable=False)
-    dta_cadastro = Colunm(DateTime,Server_default=Func.now(),nulable=False)
-    dta_atualizacao = Colunm(DateTime, onupdate=func.now(), default=func.now(), nullable=False)
+    preco = Column(DECIMAL(10, 2),
+            default=0.00)
+    estoque = Column(INTEGER,
+                     default=0)
+    ativo = Column(Boolean,
+                   default=True)
+    categoria_id = Column(Uuid(as_uuid=True),
+                          ForeignKey("tbl_categorias.id"))
 
-    lista_de_Produtos = relationship("Produto", back_populates="categoria", cascade="all,delete-orphan",
-                                     lazy="selectin")
-
-
-
-
-
-
-
-
-
-
-class Produto(Base):
-    __tablename__ = "tbl_produtos"
-
-
-
-    categoria = relationship("Categoria", back_populates="lista_de_produtos")
-
+    categoria = relationship("Categoria",
+                             back_populates="listas_de_produtos")
 
